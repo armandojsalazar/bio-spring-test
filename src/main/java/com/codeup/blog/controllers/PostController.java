ea19db25efba8faf482b1fc94db4847119648578
armandojsalazar <armando.salazar89@gmail.com>
2020-11-30 13:54:07 -0600

    feat: added a Post class and added to the PostController. Added index.html and show.html for PostController

--- src/main/java/com/codeup/blog/controllers/PostController.java
+++ src/main/java/com/codeup/blog/controllers/PostController.java
@@ -1,24 +1,34 @@
 package com.codeup.blog.controllers;
 
+import com.codeup.blog.models.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+
 @Controller
 public class PostController {
 
     @GetMapping("/posts")
-    @ResponseBody
-    public String index(){
-        return "post index page";
+    public String index(Model model){
+        ArrayList<Post> allPost = new ArrayList<Post>();
+        Post newPost1 = new Post("Codeup", "Is an awesome school!", 1, "Mando");
+        Post newPost2 = new Post("UTSA", "Is okay I guess!", 2, "Sally");
+        allPost.add(newPost1);
+        allPost.add(newPost2);
+        model.addAttribute("posts", allPost);
+        return "/posts/index";
     }
 
     @GetMapping("/posts/{id}")
-    @ResponseBody
-    public String show(@PathVariable long id){
-        return "View individual post, showing id: " + id;
+    public String Post(@PathVariable long id, Model model){
+        Post newPost = new Post("Codeup", "Is an awesome school!", id, "Bob");
+        model.addAttribute("post", newPost);
+        return "/posts/show";
     }
 
     @GetMapping("/posts/create")
--- src/main/java/com/codeup/blog/models/Post.java
+++ src/main/java/com/codeup/blog/models/Post.java
@@ -4,10 +4,15 @@ package com.codeup.blog.models;
 public class Post {
     private String title;
     private String body;
+    private long id;
+    private String author;
 
-    public Post(String title, String body){
+
+    public Post(String title, String body, long id, String author){
         this.title = title;
         this.body = body;
+        this.id = id;
+        this.author = author;
     }
 
     public String getTitle() {
@@ -24,5 +29,21 @@ public class Post {
 
     public void setBody(String body) {
         this.body = body;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
     }
 }
